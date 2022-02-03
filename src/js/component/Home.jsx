4ab882d54e92a9a3518ce54dc574ca12e84ba20b
jseiglie <javierseiglie@gmail.com>
2022-02-03 13:25:04 +0000

    counter made

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Tile from "./Tile.jsx";
 
 //create your first component
 const Home = () => {
-	return <div></div>;
+	const [Time, setTime] = useState(0);
+	useEffect(() => {
+		setInterval(() => {
+			setTime((Time) => Time + 1);
+		}, 1000);
+	}, []);
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<Tile icon="fas fa-clock" />
+					</div>
+					<div className="col">
+						<Tile inner={Time} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Home;
--- src/js/component/Tile.jsx
+++ src/js/component/Tile.jsx
@@ -1,2 +1,22 @@
-import React from 'react'
-import '../../styles/Tiles.css'
+import React from "react";
+import "../../styles/Tile.css";
+import * as PropTypes from "prop-types";
+import propTypes from "prop-types";
+
+const Tile = (props) => {
+	return (
+		<>
+			<div className="holder">
+				<i className={props.icon}></i>
+			</div>
+			<div className="holder">
+				<span>{props.inner}</span>
+			</div>
+		</>
+	);
+};
+Tile.propTypes = {
+	icon: PropTypes.string,
+	inner: PropTypes.number,
+};
+export default Tile;
